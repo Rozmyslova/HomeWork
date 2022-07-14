fb6b959a4c0285ae7ef1950b30e98dffd7b0d767
Dasha <rozmyslova.d@yandex.ru>
2022-07-14 21:42:20 +0300

    Rename folder

---
+++ HW2task/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Enter number1 = ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Enter number2 = ");
+int number2 = int.Parse(Console.ReadLine());
+int max = number1;
+if (number2 > max) {
+    Console.WriteLine("Max = " + number2);
+}
+else {
+    Console.WriteLine("Max = " + number1);
+}
