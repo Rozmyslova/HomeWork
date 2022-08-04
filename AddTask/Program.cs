413ab6b9c463c36bdf76aecb8596fbac120d99ae
Dasha <rozmyslova.d@yandex.ru>
2022-08-04 21:52:46 +0300

    Added additional tasks

---
+++ AddTask/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Не используя встроенных функций, найти с точностью до десятых квадратный корень 
+введенного пользователем числа. Число должно целым в диапазоне от 1 до 100. */
+
+Console.Write("Enter a number in rande [1; 100]: Number = ");
+double number = double.Parse(Console.ReadLine());
+double i = 1;
+while ((i * i) < number)
+{
+    i = i + 0.05;
+}
+if (((int) (i * 100 % 10)) == 5)
+{
+    i = i - 0.05;
+}
+Console.WriteLine(Math.Round(i, 1));
---
+++ AddTask2version/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Не используя встроенных функций, найти с точностью до десятых квадратный корень 
+введенного пользователем числа. Число должно целым в диапазоне от 1 до 100.
+C учетом метода Ньютона */
+
+Console.Write("Enter a number in rande [1; 100]: Number = ");
+double number = double.Parse(Console.ReadLine());
+double sqrt = 1;
+while (true)
+{
+    double number2 = (sqrt + number / sqrt) / 2;
+    if (Math.Abs(sqrt - number2) < 0.01) 
+    {
+        break;
+    }
+    sqrt = number2;
+}
+Console.WriteLine(Math.Round(sqrt, 1));
