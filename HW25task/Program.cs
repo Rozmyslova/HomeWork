e552cd4d9e06fb8b45eb4bf95edb5e2521f89ce2
Dasha <rozmyslova.d@yandex.ru>
2022-08-04 13:08:21 +0300

    Corrected task25

--- HW25task/Program.cs
+++ HW25task/Program.cs
@@ -5,5 +5,10 @@ Console.Write("Enter number A: A = ");
 int A = int.Parse(Console.ReadLine());
 Console.Write("Enter number B: B = ");
 int B = int.Parse(Console.ReadLine());
-int result = Convert.ToInt32(Math.Pow(A, B));
+// int result = Convert.ToInt32(Math.Pow(A, B));
+int result = 1;
+for (int i = 0; i < B; i++)
+{
+    result = result * A;
+}
 Console.WriteLine("A raised to B power = " + result);
