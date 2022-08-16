4d61f6655946cd901f276698162ddace155cba4b
Dasha <rozmyslova.d@yandex.ru>
2022-08-16 09:19:51 +0300

    Added new tasks

---
+++ HW41task/Program.cs
@@ -0,0 +1,14 @@
+﻿/* Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+сколько чисел больше 0 ввёл пользователь. */
+
+Console.Write("Enter M numbers separated by a space: ");
+int[] array = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine("The numbers of even elements of an array: count = " + count);
---
+++ HW43task/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Напишите программу, которая найдёт точку пересечения двух прямых, 
+заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+значения b1, k1, b2 и k2 задаются пользователем. */
+
+Console.Write("Enter k1 = ");
+double k1 = double.Parse(Console.ReadLine());
+Console.Write("Enter b1 = ");
+double b1 = double.Parse(Console.ReadLine());
+Console.Write("Enter k2 = ");
+double k2 = double.Parse(Console.ReadLine());
+Console.Write("Enter b2 = ");
+double b2 = double.Parse(Console.ReadLine());
+if ((k1 == k2) && (b1 == b2))
+{
+    Console.WriteLine("Lines match");
+}
+else if (k1 == k2)
+{
+    Console.WriteLine("Lines don`t intersect, they`re parallel");
+}
+else
+{
+    double x = Math.Round(((b2-b1)/(k1-k2)), 1);
+    double y = k1*x + b1;
+    Console.WriteLine("Intersection point of two line: (" + x + "; " + y + ")");
+}
